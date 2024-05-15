1e22e85b8d2218445e9f7f16755a263ed6884948
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-15 19:21:45 -0300

    Criação do controller `AuthorController` através do comando `php artisan make:controller --resource --api`. Incluído o primeiro método `store`.

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Método de gravação de um novo Autor  
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'date_of_birth' => 'required|date',
+        ]);
+
+        $author = Author::create([
+            'name' => $request->name,
+            'date_of_birth' => $request->date_of_birth,
+        ]);
+
+        return response()->json($author, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
