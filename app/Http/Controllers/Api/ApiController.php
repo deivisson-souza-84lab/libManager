8174fed16e0f01f5bcb3a4c328f20698d163dc33
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-15 13:09:43 -0300

    Criação do controller de autenticação da API

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    /**
+     * Método de registro de novos usuários na API
+     */
+    public function register(Request $request)
+    {
+    }
+
+    /**
+     * Método de login de ususários na API
+     */
+    public function login(Request $request)
+    {
+    }
+
+    /**
+     * Método de visualização do perfil de usuário (JWT Auth Token)
+     */
+    public function profile()
+    {
+    }
+
+    /**
+     * Método de Refresh Token da API (JWT Auth Token)
+     */
+    public function refreshToken()
+    {
+    }
+
+    /**
+     * Método de encerramento de sessão da API (JWT Auth Token)
+     */
+    public function logout()
+    {
+    }
 }
