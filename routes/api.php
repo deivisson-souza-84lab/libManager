2ebc7f114ec9cf17d84f9586e7a15b8c6b521f28
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-15 19:22:25 -0300

    Criação das rotas para `Authors`

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,8 +14,6 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('profile', [ApiController::class, 'profile']);
     Route::get('refresh-token', [ApiController::class, 'refreshToken']);
     Route::get('logout', [ApiController::class, 'logout']);
-});
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+    Route::apiResource('authors', AuthorController::class);
+});
