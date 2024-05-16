dce6531d81815aee711219a7789935c73a7122a6
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-15 22:44:45 -0300

    Criação da model para a tabela books.

---
+++ app/Models/Book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'publication_year'];
+
+    protected $dates = ['deleted_at'];
+}
