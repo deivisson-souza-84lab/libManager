30bfd3998dacd58f19582eae92f4fc864f920253
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-15 22:29:45 -0300

    Inclusão do método `update`.

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -75,9 +75,34 @@ class AuthorController extends Controller
 
     /**
      * Update the specified resource in storage.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
+        // Primeiro vamos buscar o autor pelo seu ID
+        $author = Author::find($id);
+
+        // Caso o autor não exista, vamos retornar um erro informando que ele não foi encontrado.
+        if (!$author) {
+            return response()->json([
+                'message' => 'Autor não encontrado.'
+            ], 404);
+        }
+
+        // Se ele existe, vamos fazer a alteração que precisamos. 
+        // Utilizamos a regra `sometimes` para dizer que o campo só deve ser validado caso ele exista.
+        $request->validate([
+            'name' => 'sometimes|required|string|max:255|unique:authors',
+            'date_of_birth' => 'sometimes|required|date',
+        ]);
+
+        // Com os campos validados, fazemos a gravação para os campos que existem através do método `$request->only()`
+        $author->update($request->only(['name', 'date_of_birth']));
+
+        // Após as modificações serem salvas, retornamos o conteúdo de `$author`.
+        return response()->json($author);
     }
 
     /**
