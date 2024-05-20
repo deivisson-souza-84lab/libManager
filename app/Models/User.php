713ad8c659691bf2640fbf6a046e932330c1b887
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-20 03:35:28 -0300

    inclusção de relacionamento entre a tabela users e loans.

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function loans()
+    {
+        return $this->hasMany(Loan::class);
+    }
 }
