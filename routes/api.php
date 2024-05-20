bf6e529eaec6e3ebbaee91017767ad01e1fd1bf3
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-20 03:38:54 -0300

    Remoção da classe Illuminate\Http\Request que não foi utilizada.

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Api\AuthorsController;
 use App\Http\Controllers\Api\BooksController;
 use App\Http\Controllers\Api\LoansController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Rotas sem verificação
@@ -13,11 +12,11 @@ Route::post('login', [ApiController::class, 'login']);
 
 // Rotas protegidas por autenticação
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('profile', [ApiController::class, 'profile']);
-    Route::get('refresh-token', [ApiController::class, 'refreshToken']);
-    Route::get('logout', [ApiController::class, 'logout']);
+  Route::get('profile', [ApiController::class, 'profile']);
+  Route::get('refresh-token', [ApiController::class, 'refreshToken']);
+  Route::get('logout', [ApiController::class, 'logout']);
 
-    Route::apiResource('authors', AuthorsController::class);
-    Route::apiResource('books', BooksController::class);
-    Route::apiResource('loans', LoansController::class);
+  Route::apiResource('authors', AuthorsController::class);
+  Route::apiResource('books', BooksController::class);
+  Route::apiResource('loans', LoansController::class);
 });
