ac6e118aa272298f322cb8094e7ea6d17b1d345b
deivisson-souza-84lab <deivisson.souza@84lab.com.br>
2024-05-28 18:21:51 -0300

    Criei uma função na UserFactory para permitir a criação de users admins.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -41,4 +41,13 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'role' => 'admin',
+            ];
+        });
+    }
 }
